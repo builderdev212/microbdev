cc03ce34339533c10f8197c01a26ea3191e2676b
builderdev212 <builderdev212@gmail.com>
2026-06-29 19:59:10 -0400

    test cocotb

--- verif/cocotb/scripts/coverage.sh
+++ verif/cocotb/scripts/coverage.sh
@@ -3,7 +3,7 @@ MYDIR=$(pwd)
 files=$(find . -type f -name 'coverage_test*.dat')
 echo "$files"
 verilator_coverage --write-info coverage.info $files
-cd ../../../../
-lcov_cobertura verif/cocotb/tests/$MODULE_NAME/coverage.info -o verif/cocotb/tests/$MODULE_NAME/coverage.xml
-awk -F 'branch-rate="|"' -v pkg="$MODULE_NAME" '/<package / && $0 ~ pkg {print "branch-rate=\"" $4 * 100 "\""; exit}' verif/cocotb/tests/$MODULE_NAME/coverage.xml
+cd ../../
+lcov_cobertura verif/cocotb/coverage.info -o verif/cocotb/coverage.xml
+awk -F 'branch-rate="|"' -v pkg="all" '/<package / && $0 ~ pkg {print "branch-rate=\"" $4 * 100 "\""; exit}' verif/cocotb/coverage.xml
 cd $MYDIR
