9f7bd0d8e5ae62259cf20530fbcbde06ecdaa6f0
builderdev212 <builderdev212@gmail.com>
2026-07-18 18:01:30 -0400

    add pll for vga

--- system/XC7A35T/build/add_led.tcl
+++ system/XC7A35T/build/add_led.tcl
@@ -16,3 +16,12 @@ set file_obj [get_files -of_objects [get_filesets sources_1] $verilog_files]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
 set_property -name "used_in" -value "synthesis" -objects $file_obj
 set_property -name "used_in_simulation" -value "0" -objects $file_obj
+
+set obj [get_filesets sources_1]
+set xci_files [list \
+                [file normalize "../../cores/vga/ip/vga_clk_25_17007_pll.xci"] \
+              ]
+add_files -norecurse -fileset $obj $xci_files
+set xci_obj [get_files -of_objects $obj $xci_files]
+set_property synth_checkpoint_mode Singular $xci_obj
+generate_target all $xci_obj
