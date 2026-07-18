7b0cfc3f6c0277372bb36e958db771203b384fc1
builderdev212 <builderdev212@gmail.com>
2026-07-18 15:05:10 -0400

    revert to original formatting

--- verif/cocotb/tests/basys3/fd_ss_driver/fd_ss_driver_test.py
+++ verif/cocotb/tests/basys3/fd_ss_driver/fd_ss_driver_test.py
@@ -1,12 +1,10 @@
 import os
 import shutil
 import pytest
-
 import cocotb
 from cocotb_tools.runner import get_runner
 from random import randint
 from filelock import FileLock
-
 from tb import TB
 
 
